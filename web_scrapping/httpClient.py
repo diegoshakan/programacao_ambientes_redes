57b5669a30fba0d3f6b0e009a7eddc55bf5cb338
Diego <diego_shakan@hotmail.com>
2019-05-26 12:28:32 -0300

    requests

--- web_scrapping/httpClient.py
+++ web_scrapping/httpClient.py
@@ -1,5 +1,11 @@
-import urllib.request
+# import urllib.request
 
-page = urllib.request.urlopen('http://todo-insight.herokuapp.com')
-for linha in page.readlines():
-    print(linha)
+# page = urllib.request.urlopen('http://todo-insight.herokuapp.com')
+# for linha in page.readlines():
+#     print(linha)
+
+import requests
+
+url = 'http://todo-insight.herokuapp.com'
+req = requests.get(url)
+print(req.text)
