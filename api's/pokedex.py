22e9bc518306b4f0f323b33403724741cc356a2a
Diego <diego_shakan@hotmail.com>
2019-06-09 13:50:21 -0300

    made a pokedex with many informations

--- api's/pokedex.py
+++ api's/pokedex.py
@@ -1,10 +1,70 @@
 import requests
 import json
 
-req = requests.get('https://pokeapi.co/api/v2/pokemon/mewtwo')
+req = requests.get('https://pokeapi.co/api/v2/pokemon/charizard')
 
 dic = json.loads(req.text)
 
-print(dic['height'])
-print(dic['moves'][0]['move']['name'])
-print(dic['moves'][0]['move']['name'])
+# # Nome
+nome = dic['forms'][0]['name'] 
+print(f'Nome: {nome}')
+# Evoluções
+
+
+# Altura
+altura = dic['height']
+print(f'Altura: {altura/10} m')
+
+# Peso
+peso = dic['weight']
+print(f'Peso: {peso/10} kg')
+
+# Índice
+indice = dic['game_indices'][0]['game_index']
+print(f'Indice na Pokedex: {indice}')
+
+# Imagens frente e trás
+img_frente = dic['sprites']['front_default']
+img_tras = dic['sprites']['back_default']
+print(f'Imagem de frente = {img_frente}')
+print(f'Imagem de frente = {img_tras}')
+
+
+
+index = dic['game_indices'][0]['game_index']
+
+req1 = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{index}/')
+dic1 = json.loads(req1.text)
+
+# # Cor
+cor = dic1['color']['name']
+print(f'Cor : {cor}')
+
+# Descrição Rápida
+
+desc_rap = dic1['genera']
+tam_list_desc_rap = len(dic1['genera'])
+
+for i in range(tam_list_desc_rap):
+    if desc_rap[i]['language']['name'] == 'en':
+        print('Descrição Rápida:', desc_rap[i]['genus'])
+        break
+
+desc_comp = dic1['flavor_text_entries']
+tam_list_desc_comp = len(desc_comp)
+
+for i in range(tam_list_desc_comp):
+    if desc_comp[i]['language']['name'] == 'en':
+        print('Descrição:', desc_comp[i]['flavor_text'])
+        break
+
+# Onde pode ser encontrado
+habitat = dic1['habitat']['name']
+print(f'Onde Encontrar: {habitat}')
+
+list_moves = (dic['moves'])
+tam_list_moves = len(list_moves)
+
+print('Golpes e Movimentos:' )
+for i in range(tam_list_moves):
+    print(list_moves[i]['move']['name'])
